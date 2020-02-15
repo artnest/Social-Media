e9ece6dbae311c5bee57a0da8534bac8b171e20b
artnest <artnest789@gmail.com>
2020-02-15 20:44:50 +0300

    Initial Commit

---
+++ Social Media/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Social Media
+//
+//  Created by Artyom Nesterenko on 15/2/20.
+//  Copyright © 2020 artnest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
